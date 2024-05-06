4b809db77f7c592abb7e9058a5ee657d9c16c7b4
Tristan Bridges <mophead4@gmail.com>
2024-05-06 18:18:27 -0500

    setup comments for main goals before moving to develop branch

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Example:
 // let fungusHP = 100;
 
+let fungusHP = 100;
+let myAP = 100;
+
 function onReady() {
     console.log("Ready to go!")
     
@@ -18,3 +21,39 @@ function onReady() {
 
 
 onReady()
+
+// ! function for handling click on Arcane Scepter attack button
+// * Should account for AP costs and HP decreases and update state variable accordingly
+// ? Ensure neither AP or HP values are negative, set to 0 if so
+// ? call render function at end to reflect state changes
+
+
+
+
+// ! function for handling click on Entangle attack button
+// * Should account for AP costs and HP decreases and update state variable accordingly
+// ? Ensure neither AP or HP values are negative, set to 0 if so
+// ? call render function at end to reflect state changes
+
+
+
+
+// ! function for handling click on Dragon Blade attack button
+// * Should account for AP costs and HP decreases and update state variable accordingly
+// ? Ensure neither AP or HP values are negative, set to 0 if so
+// ? call render function at end to reflect state changes
+
+
+
+
+// ! function for handling click on Star Fire attack button
+// * Should account for AP costs and HP decreases and update state variable accordingly
+// ? Ensure neither AP or HP values are negative, set to 0 if so
+// ? call render function at end to reflect state changes
+
+
+
+// ! Function to handle rendering state changes to the DOM
+// * Update AP & HP values rendered on the bars on top
+// ? If fungusHP = 0 change class on fungus from 'walk' to 'dead'
+// ? If myAP = 0 change class on fungus from 'walk' to 'jump' and attack buttons 'disabled' class
